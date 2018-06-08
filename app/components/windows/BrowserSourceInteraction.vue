7bf649ba1c4d5a74fed5ed724913809896b733b6
Andy Creeth <acreeth@gmail.com>
2018-06-07 18:00:04 -0700

    Implement browser interaction

---
+++ app/components/windows/BrowserSourceInteraction.vue
@@ -0,0 +1,27 @@
+<template>
+<modal-layout
+  :title="$t('Source Interaction')"
+  :content-styles="{ padding: 0 }"
+  :showControls="false">
+  <div
+    slot="content"
+    class="source-interaction-display"
+    ref="display"
+    @wheel="handleWheel"
+    @mousedown="handleMouseDown"
+    @mouseup="handleMouseUp"
+    @mousemove="handleMouseMove"
+    @keydown="handleKeyDown"
+    @keyup="handleKeyUp">
+    <display :source-id="sourceId" @outputResize="onOutputResize"/>
+  </div>
+</modal-layout>
+</template>
+
+<script lang="ts" src="./BrowserSourceInteraction.vue.ts"></script>
+
+<style lang="less" scoped>
+.source-interaction-display {
+  height: 100%;
+}
+</style>
---
+++ app/components/windows/BrowserSourceInteraction.vue.ts
@@ -0,0 +1,82 @@
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+import ModalLayout from 'components/ModalLayout.vue';
+import Display from 'components/shared/Display.vue';
+import { Inject } from 'util/injector';
+import { WindowsService } from 'services/windows';
+import { ISourcesServiceApi } from 'services/sources';
+import Util from 'services/utils';
+import { $t } from 'services/i18n';
+import windowMixin from '../mixins/window';
+
+@Component({
+  components: {
+    ModalLayout,
+    Display
+  },
+  mixins: [windowMixin]
+})
+export default class BrowserSourceInteraction extends Vue {
+  @Inject() windowsService: WindowsService;
+  @Inject() sourcesService: ISourcesServiceApi;
+
+  $refs: {
+    display: HTMLDivElement;
+  };
+
+  get sourceId() {
+    const windowId = Util.getCurrentUrlParams().windowId;
+    return this.windowsService.getWindowOptions(windowId).sourceId;
+  }
+
+  get source() {
+    return this.sourcesService.getSource(this.sourceId);
+  }
+
+  currentRegion: IRectangle = { x: 0, y: 0, width: 1, height: 1 };
+
+  onOutputResize(region: IRectangle) {
+    console.log('output resize', region);
+    this.currentRegion = region;
+  }
+
+  handleMouseDown(e: MouseEvent) {
+    this.source.mouseClick(e.button, this.eventLocationInSourceSpace(e), false);
+  }
+
+  handleMouseUp(e: MouseEvent) {
+    this.source.mouseClick(e.button, this.eventLocationInSourceSpace(e), true);
+  }
+
+  handleMouseMove(e: MouseEvent) {
+    this.source.mouseMove(this.eventLocationInSourceSpace(e));
+  }
+
+  handleWheel(e: WheelEvent) {
+    console.log(e);
+    this.source.mouseWheel(
+      this.eventLocationInSourceSpace(e),
+      {
+        x: e.deltaX,
+        y: e.deltaY
+      }
+    );
+  }
+
+  handleKeyDown(e: KeyboardEvent) {
+    console.log(e);
+    this.source.keyInput(e.key, false);
+  }
+
+  handleKeyUp(e: KeyboardEvent) {
+    this.source.keyInput(e.key, true);
+  }
+
+  eventLocationInSourceSpace(e: MouseEvent): IVec2 {
+    return {
+      x: (e.offsetX - this.currentRegion.x) / this.currentRegion.width * this.source.width,
+      y: (e.offsetY - this.currentRegion.y) / this.currentRegion.height * this.source.height
+    };
+  }
+
+}
